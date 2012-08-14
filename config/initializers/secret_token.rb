1f78a331b6779cfebe8d590ea149c521316a362e
gigoallen <gigo.allen@gmail.com>
2012-08-14 13:56:06 +0800

    Initial commit by Gigo 814

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp814::Application.config.secret_token = 'c913cc845106fecde1e6885d17cc58dd7aed18bbdfda7a321d46b60eaa37dddb63e2faecb8546000454d91cc247a574e77dcf18ba7d9f08aa2a4345ab5318118'
